8b60ece82d79ff62f4b8fc68a1fb45daa8457de6
Asad. Gharighi <a.gharighi@gmail.com>
2019-01-14 17:00:59 +0330

    Adding Data module

---
+++ src/Data.hs
@@ -0,0 +1,30 @@
+module Data
+  ( grid
+  , languages
+  ) where
+
+grid = [ "--C--------R---"
+       , "--SI--------U--"
+       , "--HASKELL----B-"
+       , "--A--A-----S--Y"
+       , "--R---B---C----"
+       , "--PHP----H-----"
+       , "----S-LREP-----"
+       , "----I--M-Y--L--"
+       , "----L-E--T-O---"
+       , "---------HB----"
+       , "---------O-----"
+       , "--------CN-----"
+       ]
+
+languages = [ "BASIC"
+            , "COBOL"
+            , "CSHARP"
+            , "HASKELL"
+            , "LISP"
+            , "PERL"
+            , "PHP"
+            , "PYTHON"
+            , "RUBY"
+            , "SCHEME"
+            ]
