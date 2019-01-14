4e0a943e765e9bce2a26a739d3b048fb62a711dc
Asad. Gharighi <a.gharighi@gmail.com>
2019-01-14 14:19:24 +0330

    Finding words in regular and reverse forms

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,58 @@
 module Lib
-    ( someFunc
+    ( grid
+    , languages
+    , formatGrid
+    , outputGrid
+    , findWord
+    , findWords
+    , findWordInLine
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List (isInfixOf)
+import Data.Maybe (catMaybes)
+
+type Grid = [String]
+
+outputGrid :: Grid -> IO ()
+outputGrid grid = putStrLn $ formatGrid grid
+
+formatGrid :: Grid -> String
+formatGrid = unlines
+
+findWord :: Grid -> String -> Maybe String
+findWord grid word =
+  let lines = grid ++ (map reverse grid)
+      found = or $ map (findWordInLine word) lines
+  in if found then Just word else Nothing
+
+findWords :: Grid -> [String] -> [String]
+findWords grid words = catMaybes $ map (findWord grid) words
+
+findWordInLine :: String -> String -> Bool
+findWordInLine = isInfixOf
+
+grid = [ "--C--------R---"
+       , "--SI--------U--"
+       , "--HASKELL----B-"
+       , "--A--A-----S--Y"
+       , "--R---B---C----"
+       , "--PHP----H-----"
+       , "----S-LREP-----"
+       , "----I--M-Y--L--"
+       , "----L-E--T-O---"
+       , "---------HB----"
+       , "---------O-----"
+       , "--------CN-----"
+       ]
+
+languages = [ "BASIC"
+            , "COBOL"
+            , "CSHARP"
+            , "HASKELL"
+            , "LISP"
+            , "PERL"
+            , "PHP"
+            , "PYTHON"
+            , "RUBY"
+            , "SCHEME"
+            ]
